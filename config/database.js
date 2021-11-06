6beb50d62ceb910c3701704b1f5e562b9b4a8b7a
filiafobico <oliveira1eduardo2luis3@gmail.com>
2021-11-06 11:14:12 -0300

    use mongodb

--- config/database.js
+++ config/database.js
@@ -2,17 +2,14 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', 'mariadb'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false),
+        uri: env('DATABASE_URI'),
       },
-      options: {}
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      }
     },
   },
 });
